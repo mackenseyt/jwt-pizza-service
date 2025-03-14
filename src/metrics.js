cb8f7a23ad04c130ad2555de32f4cc44c0abe9fa
mackenseyt <mackenseyathomason@gmail.com>
2025-03-14 16:35:49 -0600

    given code for del 8

---
+++ src/metrics.js
@@ -0,0 +1,31 @@
+const os = require('os');
+
+function getCpuUsagePercentage() {
+  const cpuUsage = os.loadavg()[0] / os.cpus().length;
+  return cpuUsage.toFixed(2) * 100;
+}
+
+function getMemoryUsagePercentage() {
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  const usedMemory = totalMemory - freeMemory;
+  const memoryUsage = (usedMemory / totalMemory) * 100;
+  return memoryUsage.toFixed(2);
+}
+function sendMetricsPeriodically(period) {
+    const timer = setInterval(() => {
+      try {
+        const buf = new MetricBuilder();
+        httpMetrics(buf);
+        systemMetrics(buf);
+        userMetrics(buf);
+        purchaseMetrics(buf);
+        authMetrics(buf);
+  
+        const metrics = buf.toString('\n');
+        this.sendMetricToGrafana(metrics);
+      } catch (error) {
+        console.log('Error sending metrics', error);
+      }
+    }, period);
+  }
