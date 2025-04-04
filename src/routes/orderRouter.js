e6157a713cdb84c347ae7c890ab42bc96647bce3
mackenseyt <mackenseyathomason@gmail.com>
2025-04-04 08:29:30 -0600

    Revert "chaos testing"
    This reverts commit b8de711053041be6cbb8e9619b16e8aa655345b5.

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -73,25 +73,7 @@ orderRouter.get(
     res.json(await DB.getOrders(req.user, req.query.page));
   })
 );
-let enableChaos = false;
-orderRouter.put(
-  '/chaos/:state',
-  authRouter.authenticateToken,
-  asyncHandler(async (req, res) => {
-    if (req.user.isRole(Role.Admin)) {
-      enableChaos = req.params.state === 'true';
-    }
 
-    res.json({ chaos: enableChaos });
-  })
-);
-
-orderRouter.post('/', (req, res, next) => {
-  if (enableChaos && Math.random() < 0.5) {
-    throw new StatusCodeError('Chaos monkey', 500);
-  }
-  next();
-});
 // createOrder
 orderRouter.post(
   '/',
@@ -128,6 +110,5 @@ orderRouter.post(
     }
   })
 );
-
 
 module.exports = orderRouter;
